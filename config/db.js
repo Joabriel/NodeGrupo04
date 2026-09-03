cec12fd5ccd60f3dc65c68eb8fa3e7e937426f17
Cristian Are <arevalocristian632@gmail.com>
2026-09-03 15:15:49 -0300

    feat: initialize database connection and product schema model

--- config/db.js
+++ config/db.js
@@ -11,3 +11,18 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+
+// //const mongoose = require('mongoose');
+
+// // const connectDB = async () => {
+// //     try {
+// //         await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/nodegrupo04');
+// //         console.log('MongoDB conectado exitosamente');
+// //     } catch (error) {
+// //         console.error('Error al conectar con MongoDB:', error);
+// //         process.exit(1);
+// //     }
+// // };
+
+// //module.exports = connectDB;
